310bb11f7637b9a830084378ec8c72a4166827a0
RMP <ruthiepina@gmail.com>
2024-03-24 00:28:33 -0600

    CANNOT FIGURE OUT MY MONGODB CONNECTION!!! trying with a tutor this weekend, keep getting "MongooseServerSelectionError: connect ECONNREFUSED 127.0.0.1:27017"....

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// const connectionStringURI = `mongodb://127.0.0.1:27017`;
+// const client = new MongoClient(connectionStringURI);
+// let db;
+// const dbName = "socialNetworkDB";
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,13 +18,12 @@ app.use(express.static("public"));
 app.use(require("./routes"));
 
 mongoose
-   .connect(process.env.MONGODB_URI || "mongodb://localhost/social-network", {
+   .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/social-network-api", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
    })
    .catch((err) => console.error(err));
 
-// Use this to log mongo queries being executed!
 mongoose.set("debug", true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
